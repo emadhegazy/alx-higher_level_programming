cdab7fdf225011b44e39eae6512946208e63cb40
emadhegazy <acc.ekh1@gmail.com>
2023-09-07 09:37:48 -0700

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Handle basic mathimatic  operations."""
+    """Handle basic operations."""
     from calculator_1 import add, sub, mul, div 
     import sys
 
