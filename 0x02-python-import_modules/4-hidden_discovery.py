251fd020f698bbedcd473117d21eec2927371470
emadhegazy <acc.ekh1@gmail.com>
2023-09-07 09:28:13 -0700

    :done:

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@
 if __name__ == "__main__":
     """print all fun_names that are defined in hidden_4 module"""
     import hidden_4
+
     fun_names = dir(hidden_4)
     for name in fun_names:
         if name[:2] != "__":
