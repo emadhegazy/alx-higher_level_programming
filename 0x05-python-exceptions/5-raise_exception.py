e9de32b5fe0d5574fd5d68e5d332fd2ef7979fb3
emadhegazy <acc.ekh1@gmail.com>
2023-09-25 03:03:59 -0700

    done

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def raise_exception():
+    """this is to Raise a TypeError exception."""
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def raise_exception_msg(message=""):
+    """this is to Raise a NameError exception with a message."""
+    raise NameError(message)
